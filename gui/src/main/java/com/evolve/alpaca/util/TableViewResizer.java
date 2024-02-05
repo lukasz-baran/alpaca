39d32e457d9c8af621abd1b7f0dfa65dce6f57e0
lukasz <lukasz.baran@gmail.com>
2024-02-05 17:55:48 +0100

    dynamically adjust number of rows in tableviews - adding javadoc

--- gui/src/main/java/com/evolve/alpaca/util/TableViewResizer.java
+++ gui/src/main/java/com/evolve/alpaca/util/TableViewResizer.java
@@ -5,7 +5,12 @@ import javafx.scene.control.*;
 
 public class TableViewResizer {
 
-    // this sucks because on other machines it doesn't have to be 25 pixels :)
+
+    /**
+     * this sucks because on other machines it doesn't have to be 25 pixels :( <br/>
+     *  <a href="https://stackoverflow.com/questions/26298337/tableview-adjust-number-of-visible-rows">explore other solutions</a>
+     *
+     */
     public static <T> void resizeTable(TableView<T> tableView) {
         tableView.setFixedCellSize(25);
         tableView.prefHeightProperty().bind(
